61c57f0e15eb1b0d7cd8acc795ac5f4638ec90f9
ap <aryan.poduri@gmail.com>
2024-02-17 16:34:03 -0800

    Added preferences in firestore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, session, render_template, request, redirect
 import pyrebase
+import firebase_admin
+from firebase_admin import firestore, credentials
 
 app = Flask(__name__)
 
@@ -18,6 +20,9 @@ firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 app.secret_key = 'secret'
+
+cred = credentials.Certificate("./pioneerhacks-6a057-firebase-adminsdk-8a13u-c8adfd5fce.json")
+default_app = firebase_admin.initialize_app(cred)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -46,12 +51,23 @@ def login():
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    db = firestore.client()
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
         try:
             user = auth.create_user_with_email_and_password(email, password)
             session['user'] = email
+
+            preferences = {
+                "Precipitation": request.form.get('precipitation'),
+                "Temperature": request.form.get('temperature'),
+                "Humidity": request.form.get('humidity'),
+                "Wind": request.form.get('wind'),
+                "Cloud Cover": request.form.get('cloudcover'),
+                "Visibility": request.form.get('visibility')
+            }
+            db.collection(u'Users').document(email).set(preferences)
             return render_template('home.html')
         except:
             return render_template('signup.html')
